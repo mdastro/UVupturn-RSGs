2cc9912ea4912de1ee0bbe27ac62173e2e98a37f
Maria Luiza Linhares Dantas <mlldantas@pm.me>
2019-06-18 21:43:05 -0300

    updates

--- UtilitiesCodes/plot_pars.py
+++ UtilitiesCodes/plot_pars.py
@@ -65,6 +65,20 @@ plotpars5 = {'axes.linewidth': 1.,
              'figure.figsize': (15, 4)
              }
 
+# plot pars with fig. with 3 plots (3 cols) - only one line.
+plotpars3 = {'axes.linewidth': 1.,
+             'axes.labelsize': 18,
+             'xtick.labelsize': 18,
+             'ytick.labelsize': 18,
+             'legend.frameon': True,
+             'legend.framealpha': 0.85,
+             'legend.edgecolor': 'black',
+             'legend.loc': 'upper right',
+             'legend.fontsize': 12,
+             'font.size':12,
+             'figure.figsize': (15, 4)
+             }
+
 # -------------------------------------------------------------------------
 # when adding text in the middle of crowded plot:
 
